93233741b55e89d213621ac6986f7eb982f34f0a
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-06-14 20:39:47 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tailwind Initialized in React
+
+![cover-image](https://raw.githubusercontent.com/mir-hussain/react-tailwind-setup/main/tailwind.png)
+
+Step 1
+
+```bash
+ git clone https://github.com/mir-hussain/react-tailwind-setup.git
+```
+
+Step 2
+
+```bash
+ yarn / npm install
+```
---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-dark-blue bg-main bg-contain'>
+      <h1 className='text-5xl font-mono font-black text-white'>
+        Tailwindcss Initialized
+      </h1>
+    </div>
+  );
+}
+
+export default App;
